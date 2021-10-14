b2d2a71d6a12394066ee83a353a519703c710d22
Gabriel Lantin <mushymanee@gmail.com>
2021-10-14 01:36:34 -0700

    generateMarkdown function complete, need to finish license stuff

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,12 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-
-}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
   if (license === "Apache License 2.0") {
     return "https://img.shields.io/badge/license-Apache%202.0-brightgreen";
   } else if (license === "GNU GPLv2") {
@@ -22,16 +16,64 @@ function renderLicenseLink(license) {
   }
 }
 
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  
+}
+
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  return ``
+
+  return `## License
+  
+  This application is covered under ${license}. For more information, 
+
+  `
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description 
+
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [License](#license)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions
+
+  Have any questions? My Github and email:
+
+  [Github](https://github.com/${data.github})  
+  Email: ${data.email}
+
+  ${renderLicenseSection(data.license)}
 `;
 }
 
